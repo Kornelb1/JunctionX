fa79cb3f4e9a7ae18dd19fece933cab2fa664f55
Owen Saunders <owen.saunders@hatless-studios.co.uk>
2023-04-05 07:01:24 +0100

    Add challenge endpoints, popular challenges, mine

--- backend/challenges/views.py
+++ backend/challenges/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django.db.models.aggregates import Count
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Challenge, Post, Participant
@@ -13,23 +14,49 @@ class ChallengeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Challenge.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = ChallengeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title"]
+
+    def get_queryset(self):
+        qs = self.queryset
+        only_my_challenges = self.request.query_params.get("my_challenges", None)
+        if only_my_challenges:
+            me = self.request.user
+            # Filter by challenges which I am a participant
+            my_challenges = Participant.objects.filter(user=me).values_list(
+                "challenge", flat=True
+            )
+
+            # Filter posts by those challenges
+            qs = qs.filter(pk__in=my_challenges)
+            return qs
+
+        only_popular = self.request.query_params.get("popular", None)
+        if only_popular:
+            # Order posts by number of participants
+            # Participant.objects.filter(challenge=obj.pk).count()
+            qs = qs.annotate(num_participants=Count('participants'))
+            qs = qs.order_by("-num_participants")[:6]
+            return qs
+
+        return super().get_queryset()
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
-    search_fields = ['title']
 
     def get_queryset(self):
         qs = self.queryset
-        print(self.request)
         # All posts for all my challenges
         only_my_challenges = self.request.query_params.get("my_challenges", None)
         if only_my_challenges:
             me = self.request.user
             # Filter by challenges which I am a participant
-            my_challenges = Participant.objects.filter(user=me).values_list("challenge", flat=True)
+            my_challenges = Participant.objects.filter(user=me).values_list(
+                "challenge", flat=True
+            )
 
             # Filter posts by those challenges
             qs = qs.filter(challenge__in=my_challenges)
